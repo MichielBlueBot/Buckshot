3892edad16fb52cf5172936de4442bfea485e6fe
MichielBlueBot <michielruelens@gmail.com>
2015-05-30 17:56:40 +0200

    First attempt of incorporating Photon Unity Networking. Movement working, animation NOT working. Fireballs still TODO.

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	private const string GAME_VERSION = "v0.0.1";
+	public string ROOM_NAME = "BuckshotRoomDefault";
+
+	private int nextSpawnedPlayerTeam = 1;
+
+	public Transform spawn1;
+	public Transform spawn2; 
+
+	// All these prefabs need to be in the Prefabs/Resources/ root folder.
+	public string playerPrefabName = "Player";
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings (GAME_VERSION);
+	}
+
+	void OnJoinedLobby(){
+		RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 4 };
+		PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
+	}
+
+	void OnJoinedRoom(){
+		if (nextSpawnedPlayerTeam == 1) {
+			PhotonNetwork.Instantiate (playerPrefabName, spawn1.position, spawn1.rotation, 0);
+			nextSpawnedPlayerTeam = 2;
+		} else {
+			PhotonNetwork.Instantiate (playerPrefabName, spawn2.position, spawn1.rotation, 0);
+			nextSpawnedPlayerTeam = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters;
+
+public class NetworkPlayer : Photon.MonoBehaviour {
+
+	public GameObject playerCamera;
+	public GameObject playerCharacter;
+
+	private bool isAlive = true;
+	private Vector3 position;
+	private Quaternion rotation;
+	private float lerpSmoothing = 5;
+
+	// Use this for initialization
+	void Start () {
+		if (photonView.isMine) {
+			// Local player
+			playerCamera.SetActive (true);
+			playerCharacter.GetComponent<ThirdPersonMouseControl>().enabled = true;
+		} else {
+			// Remote Network player
+			StartCoroutine("Alive");
+		}
+	}
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
+		if (stream.isWriting) {
+			stream.SendNext(playerCharacter.transform.position);
+			stream.SendNext(playerCharacter.transform.rotation);
+		} else {
+			position = (Vector3) stream.ReceiveNext();
+			rotation = (Quaternion) stream.ReceiveNext();
+		}
+	}
+
+	//while alive do this
+	IEnumerator Alive(){
+		while (isAlive) {
+			// Update the position and rotation
+			playerCharacter.transform.position = Vector3.Lerp (playerCharacter.transform.position, position, Time.deltaTime*lerpSmoothing);
+			playerCharacter.transform.rotation = Quaternion.Lerp (playerCharacter.transform.rotation, rotation, Time.deltaTime*lerpSmoothing);
+			//TODO FIX THIS
+			//TODO FIX THIS
+			//TODO FIX THIS
+			//TODO >>>>>>>>>>>
+			// Update the animator
+			/*
+			Vector3 movementVector = position-playerCharacter.transform.position;
+			movementVector.Normalize();
+			playerCharacter.transform.FindChild("AIThirdPersonController").GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>().RemoteUpdateAnimator(movementVector);
+			*/
+			//TODO <<<<<<<<<<<
+			//TODO FIX THIS
+			//TODO FIX THIS
+			//TODO FIX THIS
+
+
+			yield return null;
+		}
+	}
+}
